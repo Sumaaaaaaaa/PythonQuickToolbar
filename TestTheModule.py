787f5c4deeaa1d8bc3b6d21c79178cf3cc2dbf62
Sumaaaaaaaa <a1105504813@gmail.com>
2024-02-29 23:34:20 +0900

    System optimize
    Optimize the system architecture to facilitate future development.

--- TestTheModule.py
+++ TestTheModule.py
@@ -1,16 +1,22 @@
 import quicktoolbar
+from PIL import Image
 
 #quicktoolbar.root.ManageWindow_test()
 def test():
-    a = "A path from a point approximately 330 metres east of the most south westerly corner of 17 Batherton Close, Widnes and approximately 208 metres east-south-east of the most southerly corner of Unit 3 Foundry Industrial Estate, Victoria Street, Widnes, proceeding in a generally east-north-easterly direction for approximately 28 metres to a point approximately 202 metres east-south-east of the most south-easterly corner of Unit 4 Foundry Industrial Estate, Victoria Street, and approximately 347 metres east of the most south-easterly corner of 17 Batherton Close, then proceeding in a generally northerly direction for approximately 21 metres to a point approximately 210 metres east of the most south-easterly corner of Unit 5 Foundry Industrial Estate, Victoria Street, and approximately 202 metres east-south-east of the most north-easterly corner of Unit 4 Foundry Industrial Estate, Victoria Street, then proceeding in a generally east-north-east direction for approximately 64 metres to a point approximately 282 metres east-south-east of the most easterly corner of Unit 2 Foundry Industrial Estate, Victoria Street, Widnes and approximately 259 metres east of the most southerly corner of Unit 4 Foundry Industrial Estate, Victoria Street, then proceeding in a generally east-north-east direction for approximately 350 metres to a point approximately 3 metres west-north-west of the most north westerly corner of the boundary fence of the scrap metal yard on the south side of Cornubia Road, Widnes, and approximately 47 metres west-south-west of the stub end of Cornubia Road be diverted to a 3 metre wide path from a point approximately 183 metres east-south-east of the most easterly corner of Unit 5 Foundry Industrial Estate, Victoria Street and approximately 272 metres east of the most north-easterly corner of 26 Ann Street West, Widnes, then proceeding in a generally north easterly direction for approximately 58 metres to a point approximately 216 metres east-south-east of the most easterly corner of Unit 4 Foundry Industrial Estate"
-    return a
+    a = Image.open("Icons\\close.png")
+    return a 
 
 quicktoolbar.createButton(
     name = "1",
     command=test,
-    returnType=quicktoolbar.ReturnType.String,
+    returnType=quicktoolbar.ReturnType.Image,
     mode = quicktoolbar.Mode.Api
     )
-quicktoolbar.creat_instant()
+quicktoolbar
 quicktoolbar.run()
 # 整理变量,创建测试文件，推进
+"""
+TODO:
+    -[] 测试returnType枚举值是否可用的isinstance方法是否能正常运行
+    -[] 当照片传入的地址错误时，是否会正常报错
+"""
